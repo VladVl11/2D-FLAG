ed2d2edd4a25542c8835069de169e3e3420c248d
TheDeadPigeon <matt.garrett913@gmail.com>
2026-02-08 23:32:36 +0000

    level gen script
    Implemented Level generation script.

---
+++ Assets/Scripts/Map Gen/Map Generation.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using UnityEngine;
+
+
+public class MapGeneration : MonoBehaviour
+{
+    public GameObject[] MapPrefabs; // Array of map prefabs
+    public int mapSize; // Size of the map (width)
+    public float mapY; // Y position for the map prefabs
+    void mapGenerator()
+    {
+        
+        float spawnPos = 0;
+        GameObject LastPrefab = null;
+        for (int i = 0; i < mapSize; i++)
+        {
+            
+            GameObject prefab = MapPrefabs[Random.Range(0, MapPrefabs.Length)];
+            if (prefab != LastPrefab)
+            {
+                LastPrefab = prefab;
+                Instantiate(prefab, new Vector3(spawnPos, mapY, 0), Quaternion.identity);
+                float length = prefab.GetComponent<PresetLength>().length;
+                spawnPos += length;
+                GameObject duplicatePrefabCheck = prefab;
+            }
+            else
+            {
+                i--;
+                continue;
+            }
+
+
+        }
+    }
+        
+
+    void Start()
+    {
+        mapGenerator();
+    }
+
+    
+}
---
+++ Assets/Scripts/Map Gen/PresetLength.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class PresetLength : MonoBehaviour
+{
+    public float length; // Length of the map prefab
+}
