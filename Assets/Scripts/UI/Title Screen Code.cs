2ca288c44f3597c0bf2e4f65576c4d1b01c38609
Danullek123 <danullekprivate@gmail.com>
2026-05-27 15:42:31 +0100

    Gameover Screen UI + Splash screen for publisher
    As said above, added those two things, ESC still opens the options menu and it isn't clickable for some reason, also, the start button now leads to a crash for some reason and I don't get it, it wasn't doing that before, please take a look when you have a chance cuz I don't know how to fix it.

--- Assets/Scripts/UI/Title Screen Code.cs
+++ Assets/Scripts/UI/Title Screen Code.cs
@@ -1,14 +1,23 @@
 using System;
+using System.Collections;
+using Unity.Hierarchy;
 using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using static UnityEngine.Rendering.DebugUI;
 
 public class TitleScreenCode : MonoBehaviour
 {
+
     public void QuitApp()
     {
         Application.Quit();
         print("The game has been closed");
+    }
+
+    public void Start()
+    {
+        
     }
 }
